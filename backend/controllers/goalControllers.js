2d7f877cadc37404ceac642973ea56e4a069b926
hacksh4w <mrinalsnair@gmail.com>
2023-06-08 06:31:03 +0530

    Goals contolleer and routes setup

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,38 +1,35 @@
+const asyncHandler = require('express-async-handler')
 // @desc GET goals
 // @rouete GET/api/goals
 // @access Private
 
-const getGoals = (req,res) => {
+const getGoals = asyncHandler(async (req,res) => {
     res.status(200).json({ message : 'Get Goalsss'})
-}
+})
 
 // @desc SET goals
 // @rouete POST /api/goals
 // @access Private
 
-const setGoal = (req,res) => {
-    if(!req.body.text){
-        res.status(400)
-        throw new Error('Please add a text')
-    }
+const setGoal = asyncHandler(async (req,res) => {
     res.status(200).json({ message : 'Set Goalsss'})
-}
+})
 
 // @desc UPDATE goals
 // @rouete PUT /api/goals/:id
 // @access Private
 
-const updateGoal = (req,res) => {
+const updateGoal = asyncHandler(async (req,res) => {
     res.status(200).json({ message : `Update Goal ${req.params.id}`})
-}
+})
 
 // @desc DELETE goals
 // @rouete DELETE /api/goals
 // @access Private
 
-const deleteGoal = (req,res) => {
+const deleteGoal = asyncHandler(async (req,res) => {
     res.status(200).json({ message : `Delete Goal ${req.params.id}`})
-}
+})
 
 
 module.exports = {
