86ed5761897828aeedc049fa97c541510c8f4a11
hacksh4w <mrinalsnair@gmail.com>
2023-06-09 06:58:24 +0530

    added login, register, error handling

---
+++ backend/controllers/userController.js
@@ -0,0 +1,76 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+
+//@desc Register new user
+//@route POST?api/users
+//@access PUBLIC
+const registerUser = asyncHandler(async (req,res) => {
+    const { name, email, password } = req.body 
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error("Please add all fields")
+    }
+
+    //Check if user exists
+    const userExists = await User.findOne({email})
+
+    if(userExists) {
+        res.status(400)
+        throw new Error( 'user LAREADY EXISTS')
+    }
+
+    //HASH PASSWORD
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt) //using hashSync made header file problem, changed to hash instead 
+
+    //create user
+    const user = await User.create({
+        name,
+        email,
+        password : hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id : user.id, 
+            name : user.name,
+            email: user.email,
+        })
+    } else  {
+        res.sendStatus(400)
+        throw new Error('Invalid User Data')
+    }
+})
+
+//@desc Authenticate a user
+//@route POST api/users/login
+//@access PUBLIC
+const loginUser = asyncHandler(async (req,res) => {
+    const {email, password} = req.body
+    //Check for user email
+    const user = await User.findOne({email})
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json ({
+            _id : user.id, 
+            name : user.name,
+            email: user.email,
+        })
+    } else {
+        res.sendStatus(400)
+        throw new Error('Invalid credentials')
+    }
+})
+
+//@desc GET user data
+//@route GET api/users/me
+//@access PUBLIC
+const getMe = asyncHandler(async (req,res) => {
+    res.json({message : 'Display User Data'})
+})
+
+module.exports = {
+    registerUser, loginUser, getMe
+}
