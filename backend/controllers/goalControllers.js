f53ac6bb6369eafe427556adb6339eea44139e5c
hacksh4w <mrinalsnair@gmail.com>
2023-06-09 05:45:26 +0530

    controllers updated

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,10 +1,15 @@
 const asyncHandler = require('express-async-handler')
+
+const Goal = require('../models/goalModel')
+
 // @desc GET goals
 // @rouete GET/api/goals
 // @access Private
 
 const getGoals = asyncHandler(async (req,res) => {
-    res.status(200).json({ message : 'Get Goalsss'})
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
 
 // @desc SET goals
@@ -12,7 +17,15 @@ const getGoals = asyncHandler(async (req,res) => {
 // @access Private
 
 const setGoal = asyncHandler(async (req,res) => {
-    res.status(200).json({ message : 'Set Goalsss'})
+   if (!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field') 
+   }
+
+    const goal = await Goal.create ({
+        text : req.body.text,
+    }) 
+    res.status(200).json(goal)
 })
 
 // @desc UPDATE goals
@@ -20,7 +33,16 @@ const setGoal = asyncHandler(async (req,res) => {
 // @access Private
 
 const updateGoal = asyncHandler(async (req,res) => {
-    res.status(200).json({ message : `Update Goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+   if(!goal)  {
+    res.status(400)
+    throw new Erro('Goal not found')
+   }
+   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+})
+
+    res.status(200).json(updatedGoal)
 })
 
 // @desc DELETE goals
@@ -28,7 +50,14 @@ const updateGoal = asyncHandler(async (req,res) => {
 // @access Private
 
 const deleteGoal = asyncHandler(async (req,res) => {
-    res.status(200).json({ message : `Delete Goal ${req.params.id}`})
+
+    const goal = await Goal.findById(req.params.id)
+    if(!goal)  {
+        res.status(400)
+        throw new Erro('Goal not found')
+    }
+    await goal.deleteOne()
+        res.status(200).json({id: req.params.id})   //instead .remove() using DeleteOne()
 })
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMIddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5500
+
+connectDB()
 
 const app = express()
 
