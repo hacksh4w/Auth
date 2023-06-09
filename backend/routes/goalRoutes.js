85fa5e701f7b26e68fb8233d48acc16a29788921
hacksh4w <mrinalsnair@gmail.com>
2023-06-09 08:12:32 +0530

    protected goal routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,10 +5,12 @@ const { getGoals,
         updateGoal, 
         deleteGoal } = require('../controllers/goalControllers')
 
+        const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(getGoals).post(setGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
 // combination of router.get('/', getGoals) && router.post('/', setGoal)
 
 //comibnation of router.put('/:id', updateGoal) && router.delete('/:id', deleteGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
+
 module.exports = router
